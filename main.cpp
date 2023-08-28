d5e012af482e15de5e0d416603bdbaedaeca7a88
Sinabb <86355292+Sinabb@users.noreply.github.com>
2023-08-28 20:40:13 +0900

    0828 study

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-bool LoadFile(const std::string& filename)
+class Monster
+{
+private:
+	std::string mName;
+	int mLevel;
+	int mHP;
+	int mMP;
+
+public:
+	Monster(std::string name, int level, int hp,int mp): mName{name},mLevel{level},mHP{hp},mMP{mp}
+	{
+	}
+	friend std::ostream& operator <<(std::ostream& os, Monster m)
+	{
+		os << m.mName << ":" << m.mLevel << "," << m.mHP << "," << m.mMP;
+		return os;
+	}
+};
+
+
+bool LoadFile(const std::string& filename,std::vector<Monster>)
+{
+	std::ifstream ifs;
+	ifs.exceptions(std::ifstream::badbit);
+
+	try
+	{
+		ifs.open(filename);
+		char ch;
+		while (ifs.get(ch))
+		{
+			std::cout << ch;
+		}
+		ifs.close();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cerr << std::endl << e.code() << "," << e.what() << std::endl;
+		ifs.close();
+		return false;
+	}
+
+	return true;
+}
+
+
+int main()
+{
+	//path(directory of folder, file)
+	/*LoadFile("C:/Users/askit/source/repos/FileSystem/Data/SimpleData.txt");
+	LoadFile("./Data/SimpleData.txt");*/
+	std::vector<Monster> monsters;
+	//LoadFile("Data/SimpleData.txt", monsters);
+
+	for (const auto& e:monsters)
+	{
+		std::cout << e << std::endl;
+	}
+}
+
+/*
+	bool LoadFile(const std::string& filename)
 {
 	// file stream
 	std::ifstream ifs(filename);
@@ -33,7 +95,7 @@ bool LoadFile(const std::string& filename)
 		std::cout << ch;
 	}
 
-	/*
+	
 	while (ifs>>ch)
 	{
 		std::cout << ch;
@@ -53,17 +115,9 @@ bool LoadFile(const std::string& filename)
 	{
 		std::cout << buffer << std::endl;
 	}
-	*/
+	
 	// close file
-	ifs.close();
-	return true;
+ifs.close();
+return true;
 }
-
-
-int main()
-{
-	//path(directory of folder, file)
-	LoadFile("C:/Users/askit/source/repos/FileSystem/Data/SimpleData.txt");
-	LoadFile("./Data/SimpleData.txt");
-	LoadFile("Data/SimpleData.txt");
-}
+*/
